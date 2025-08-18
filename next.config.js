0577325f04d931cdb81628522edee65a2b8c3554
David Mendez <saurionx2005@gmail.com>
2025-08-18 20:11:30 -0300

    mi

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  output: 'export', // Habilita la exportación estática
+  // Opcional: Cambia la carpeta de salida (por defecto es 'out')
+  distDir: 'dist', // Si quieres que los archivos se guarden en /dist en lugar de /out
+};
